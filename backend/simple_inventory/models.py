461177cf9a412757a64439f255165e9f23ce3759
Callum Kendrick <callumkendrick@hotmail.co.uk>
2025-03-10 16:35:47 +0000

    Add item to model

--- backend/simple_inventory/models.py
+++ backend/simple_inventory/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity}"
