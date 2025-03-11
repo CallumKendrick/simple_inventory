1e768f8c97f9aa8ab66c6218d5586566868a6200
Callum Kendrick <callumkendrick@hotmail.co.uk>
2025-03-11 16:11:56 +0000

    Fix Item serializer
    Class definition was missing a colon

--- backend/simple_inventory/serializers.py
+++ backend/simple_inventory/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Item
 
-class ItemSerializer(serializers.ModelSerializer)
+class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ['id', 'name', 'quantity']
